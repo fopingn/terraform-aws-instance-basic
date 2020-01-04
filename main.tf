2ac410805d23b02552c032c5ebcf3976881a69a7
fopingn <fopingn@gmail.com>
2020-01-04 08:12:13 -0500

    Minor update

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 resource "aws_instance" "this" {
-  #count = var.instance_count
-
   ami                         = var.ami
   instance_type               = var.instance_type
   monitoring                  = var.monitoring
